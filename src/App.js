825eb1fd28a7b5006ffee023f665fb61a2bf9d88
munimahmed1991 <munimahmed1991@gmail.com>
2022-04-05 17:43:03 +0500

    add work

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let a = 0;
+  let add=() => {
+    a =a+1;
+    a =2;
+  };  // function add(c){ return c}
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Hello React </h1>
+        <button onClick={add} >Add</button>
+        {a}
+
+
       </header>
     </div>
   );
